21541c318dbb9c135611bdd0441b6b5d8e3f75e4
Sotheanith Sok <thetheanith@gmail.com>
2018-10-15 20:29:44 -0700

    Format the code

--- src/decapsulator.js
+++ src/decapsulator.js
@@ -2,35 +2,36 @@ const crypto = require('crypto');
 const fs = require('fs');
 
 // Class definition
-class Decapsulator{
-       constructor(keyPath){
-           //Read private key
-        try{
-            this.privateKey=fs.readFileSync(keyPath,'utf8');
-        }catch(e){
+class Decapsulator {
+    constructor(keyPath) {
+        //Read private key
+        try {
+            this.privateKey = fs.readFileSync(keyPath, 'utf8');
+        } catch (e) {
             console.log(e);
         }
+
     }
 
-//funtion to decapsulate asekey and tag.
-//function also decrypt the ciphertext.
-  decrypt(encrypted){
-      
-    const Algorithm = 'aes-256-gcm';
+    //funtion to decapsulate asekey and tag.
+    //function also decrypt the ciphertext.
+    decrypt(encrypted) {
 
-    let asekey = crypto.privateDecrypt(this.privateKey,Buffer.from(encrypted.key,'hex'));
-    let tag = crypto.privateDecrypt(this.privateKey,Buffer.from(encrypted.tag,'hex'));
+        const Algorithm = 'aes-256-gcm';
 
-    //extract the first 32 bytes from the content to get the iv.
-    let iv =Buffer.from(encrypted.content.substring(0,32),'hex');
+        let asekey = crypto.privateDecrypt(this.privateKey, Buffer.from(encrypted.key, 'hex'));
+        let tag = crypto.privateDecrypt(this.privateKey, Buffer.from(encrypted.tag, 'hex'));
 
-    //Decript the content
-    var decipher = crypto.createDecipheriv(Algorithm, asekey, iv)
-    decipher.setAuthTag(tag);
-    var decrypt = decipher.update(encrypted.content.substring(32),'hex','utf8')
-    decrypt += decipher.final('utf8');
-    return decrypt;
-  }
-    
-}module.exports=Decapsulator;
+        //extract the first 32 bytes from the content to get the iv.
+        let iv = Buffer.from(encrypted.content.substring(0, 32), 'hex');
+
+        //Decript the content
+        var decipher = crypto.createDecipheriv(Algorithm, asekey, iv)
+        decipher.setAuthTag(tag);
+        var decrypt = decipher.update(encrypted.content.substring(32), 'hex', 'utf8')
+        decrypt += decipher.final('utf8');
+        return decrypt;
+    }
+
+} module.exports = Decapsulator;
 

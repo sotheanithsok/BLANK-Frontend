196a7da0180e82a1237b6939a09ec01598b7186b
Sotheanith Sok <thetheanith@gmail.com>
2018-11-06 11:22:16 -0800

    update path

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ const { app, BrowserWindow } = require('electron')
       win = new BrowserWindow({ width: 800, height: 600 })
     
       // and load the index.html of the app.
-      win.loadFile('index.html')
+      win.loadFile('./src/html/index.html')
     
       // Open the DevTools.
       win.webContents.openDevTools()

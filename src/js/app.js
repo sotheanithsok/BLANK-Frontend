d40b183f6e8e0fc067c8916e2301db014457ef5e
Sotheanith Sok <thetheanith@gmail.com>
2018-11-25 14:50:56 -0800

    add proxy for messages and users. Need to fix users remove proxy

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,183 @@
+//HTML elements
 const messagesContainer = document.getElementById('messages-container');
 const usersContainer = document.getElementById('users-container');
 const messageInput = document.getElementById('message-input-box');
 const userSearchInput = document.getElementById('search-user-field');
-const searchResultContainer=document.getElementById('search-result-container');
+const searchResultContainer = document.getElementById('search-result-container');
+
+//Proxy objects
+let messages = new Proxy([], {
+
+    /**
+     * Override array set method to automatically create html message element when new item are added to array.
+     * @param {*} target
+     * @param {*} property 
+     * @param {*} value 
+     * @param {*} receiver 
+     */
+    set(target, property, value, receiver) {
+        if (value.hasOwnProperty('sender') && value.hasOwnProperty('message') && value.sender.trim() && value.sender.trim()) {
+            value.timestamp = Date.now();
+            target[property] = value;
+            createMessageElement(value.sender, value.message, value.timestamp);
+        }
+        return true;
+    },
+
+    /**
+     * Override array set method to automatically delete html message element when an item are removed from arrray.
+     * @param {*} target 
+     * @param {*} property 
+     */
+    deleteProperty(target, property) {
+        if (property in target) {
+            removeMessageElement(target[property].sender, target[property].message, target[property].timestamp);
+            target.splice(property, 1);
+        }
+        return true;
+    }
+});
+
+let users = new Proxy([], {
+    set(target, property, value, receiver) {
+        if(property!='length'){
+            target.forEach((v)=>{
+                if(v===value){
+                    return true;
+                }
+            })
+            target[property]=value;
+            createUserElement(value);
+        }
+       
+        return true;
+    },
+    deleteProperty(target,property){
+        if(property in target){
+            removeUserElement(target[property]);
+            target.splice(property,1);
+        }
+        return true;
+    }
+});
+
+let searchResult = new Proxy({}, {
+    set: function (target, property, value, receiver) {
+        return true;
+    }
+});
 
 
-
-function startSearch(){
+function startSearch() {
     while (searchResultContainer.firstChild) {
         searchResultContainer.removeChild(searchResultContainer.firstChild);
     }
-    let i =userSearchInput.value.length;
-    while(i--){
+    let i = userSearchInput.value.length;
+    while (i--) {
         createResultedUserElement(userSearchInput.value.charAt(i))
     }
 }
 
 function sendMessage() {
-    createMessageElement("JAKE", messageInput.value);
-    messageInput.value = "";
-    createUserElement(makeid());
-
+    let found=false;
+    for(let i =0; i <users.length;i++){
+        if(users[i]===messageInput.value){
+            delete users[i];
+            found=true;
+            break;
+        }
+    }
+    if(!found){
+        users.push(messageInput.value);
+    }
+    console.log(users);
+    
 }
 
 
-function createMessageElement(name, message) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * A helper function uses to create html message element.
+ * @param {*} sender 
+ * @param {*} message 
+ * @param {*} timestamp 
+ */
+function createMessageElement(sender, message, timestamp) {
     let button = document.createElement('button');
     let text = document.createElement('pre');
     let whitespace = "";
-    for (let i = 0; i <= name.length; i++) {
+    for (let i = 0; i <= sender.length; i++) {
         whitespace += ' ';
     }
     let temp = message.replace(/^/g, whitespace);
-    text.innerHTML = name + ":\n" + temp;
+    text.innerHTML = sender + ":\n" + temp;
     button.appendChild(text);
+    button.setAttribute('tID', btoa(sender + message + timestamp));
     messagesContainer.appendChild(button);
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    messageInput.value = ""
 }
 
+/**
+ * A helper function uses to remove html message element.
+ * @param {*} sender 
+ * @param {*} message 
+ * @param {*} timestamp 
+ */
+function removeMessageElement(sender, message, timestamp) {
+    let tID = btoa(sender + message + timestamp);
+    messagesContainer.childNodes.forEach((element) => {
+        if (element.getAttribute('tID') === tID) {
+            messagesContainer.removeChild(element);
+        }
+    });
+}
+
+/**
+ * A helper function uses to create html user elements.
+ * @param {*} name 
+ */
 function createUserElement(name) {
     let button = document.createElement('button');
     button.appendChild(document.createTextNode(name));
+    button.setAttribute('tID', btoa(name));
     usersContainer.appendChild(button);
 }
 
+/**
+ * A helper function uses to remove html user elements.
+ * @param {*} name 
+ */
+function removeUserElement(name) {
+    let tID = btoa(name);
+    usersContainer.childNodes.forEach((element) => {
+        if (element.getAttribute('tID') === tID) {
+            usersContainer.removeChild(element);
+        }
+    });
+}
+
+/**
+ * A helper function uses to create html resulted user elements.
+ * @param {*} name 
+ */
 function createResultedUserElement(name) {
     let button = document.createElement('button');
     button.appendChild(document.createTextNode(name));
     searchResultContainer.appendChild(button);
 }
+

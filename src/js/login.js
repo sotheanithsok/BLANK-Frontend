0bb7159000cdc2600be1919d56af78469e183228
Sotheanith Sok <thetheanith@gmail.com>
2018-12-03 20:05:04 -0800

    Complete the integration of signup and login into our application

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,22 @@
+/**
+ * On click of the submit button, start the login process.
+ */
 function startLogin(){
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     httpRequester.login(username, password);
+}
+
+/**
+ * If login successful, call this function
+ */
+function switchToSignup(){
+    ipcRenderer.send('asynchonous-main-switchToSignup');
+}
+
+/**
+ * If login failed, alert user about it.
+ */
+function failToLogin(){
+    vex.dialog.alert('Login failed. Please check your username or password and try again.');
 }
